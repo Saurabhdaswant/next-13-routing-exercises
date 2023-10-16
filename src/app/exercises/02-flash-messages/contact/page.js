0739c22a89ae7530d9bbaf40a74f5a5e8eeb6fb9
saurabh <77434364+Saurabhdaswant@users.noreply.github.com>
2023-10-16 23:33:32 +0530

    second task completed

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,26 @@
-'use client';
-import React from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useContext } from "react";
+import { ToastContext } from "../../../../components/ToastProvider";
 
 function ContactPage() {
+  const router = useRouter();
+
+  const { createToast } = useContext(ToastContext);
+
   return (
     <main>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          router.push("/exercises/02-flash-messages");
+
+          createToast(
+            "thakns for contacting us! , we'll get back to you soon!"
+          );
+        }}
+      >
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
