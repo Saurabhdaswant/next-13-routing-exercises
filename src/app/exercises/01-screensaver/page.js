59af0230cd2188cadb7357b62def494fecd83580
saurabh <77434364+Saurabhdaswant@users.noreply.github.com>
2023-10-16 23:10:55 +0530

    complete task

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-import ScreenSaver from '../../../components/ScreenSaver';
-
-function ScreenSaverExercise() {
+export default function page() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
-    </main>
+    <div>
+      <h1>Choose Color from here love : </h1>
+      <li>
+        <Link href="/exercises/01-screensaver/blue">blue</Link>
+      </li>
+      <li>
+        <Link href="/exercises/01-screensaver/red">red</Link>
+      </li>
+      <li>
+        <Link href="/exercises/01-screensaver/green">green</Link>
+      </li>
+      <li>
+        <Link href="/exercises/01-screensaver/pink">pink</Link>
+      </li>
+    </div>
   );
 }
-
-export default ScreenSaverExercise;
